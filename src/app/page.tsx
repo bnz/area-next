4eb9d11e53a26bfe1fa1bdd84f589075b9ed760d
bonez <y.tugarinov@dynatech.lv>
2025-04-16 15:56:45 +0300

    remove console.log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 import Link from 'next/link'
 
 export default function Home() {
-
-	console.log(process.env.NEXT_PUBLIC_GITHUB_TOKEN)
-
 	return (
 		<div className="">
 			aria | <Link href={`/admin`}>admin</Link>
