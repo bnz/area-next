33ae774f1de86481f7179031c09925ac0e3ba4b0
bonez <cheekbonez@gmail.com>
2025-04-28 15:20:46 +0300

    ...

--- src/app/[lang]/admin/page.tsx
+++ src/app/[lang]/admin/page.tsx
@@ -1,4 +1,4 @@
-import { generateLangStaticParams } from "@/lib/i18n"
+import { type  AvailableLangs, generateLangStaticParams } from "@/lib/i18n"
 import { type  PageParams } from "@/app/[lang]/page"
 import { Login } from "@/components/Login"
 
@@ -7,5 +7,5 @@ export const generateStaticParams = generateLangStaticParams
 export default async function Page({ params }: PageParams) {
     const { lang } = await params
 
-    return <Login lang={lang} />
+    return <Login lang={lang as AvailableLangs} />
 }
