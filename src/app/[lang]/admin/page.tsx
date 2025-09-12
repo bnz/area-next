e9973960ce071087fef909dcbe9bd4bdd3205527
bonez <y.tugarinov@dynatech.lv>
2025-09-12 16:30:09 +0300

    ...

--- src/app/[lang]/admin/page.tsx
+++ src/app/[lang]/admin/page.tsx
@@ -1,17 +1,11 @@
 import { type  AvailableLangs, generateLangStaticParams } from "@/lib/i18n"
-import { Login } from "@/components/admin/Login"
 import { type PageProps } from "@/app/page"
-import { redirect } from "next/navigation"
 import { SetAdminActive } from "@/components/admin/SetAdminActive"
 
 export const generateStaticParams = generateLangStaticParams
 
 export default async function Page({ params }: PageProps) {
     const { lang } = await params
-
-
-
-    // return <Login lang={lang as AvailableLangs} />
 
     return <SetAdminActive lang={lang as AvailableLangs} />
 }
